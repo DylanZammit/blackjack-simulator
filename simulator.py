d1bc70fc9fa0a100ed951a8edb16554b0fe04e90
Dylan Zammit <dylanzam@gmail.com>
2024-02-17 17:53:44 +0100

    change monte carlo strategy

--- simulator.py
+++ simulator.py
@@ -31,7 +31,7 @@ class Card:
     deck = 'A23456789TJQK'
 
     def __init__(self, rank: str):
-        assert rank in Card.deck
+        assert rank in Card.deck, f'{rank} not found'
         self.rank = rank
 
     def __repr__(self):
@@ -76,6 +76,8 @@ class Hand:
         return len(self.cards)
 
     def add_card(self, card: Card) -> None:
+        if isinstance(card, str):
+            card = Card(card)
         self.cards.append(card)
 
     def __eq__(self, other):
@@ -98,7 +100,7 @@ class Hand:
         if 21 in [min_val, max_val]: return 21
         if min_val > 21: return 22
         if max_val > 21: return min_val
-        return min_val#, max_val
+        return min_val  #, max_val
 
     @property
     def num_aces(self) -> int:
@@ -154,6 +156,14 @@ class Player:
     def is_finished(self) -> bool:
         return self.status in ['WON', 'LOST', 'DRAW']
 
+    @property
+    def profit(self) -> float:
+        if self.status == GameState.WON and self.is_blackjack: return 1.5
+        if self.status == GameState.WON: return 1
+        if self.status == GameState.DRAW: return 0
+        if self.status == GameState.LOST: return -1
+        return 0
+
     def __repr__(self):
         return f'Player {self.name}: hand={self.hand} ({self.hand.value}) [{self.status}]'
 
@@ -183,7 +193,7 @@ class Blackjack:
             assert dealer_hand is not None, 'Must also specify the dealer\'s hand if the players\' hand is specified'
 
             for player, hand in zip(self.players, player_hands):
-                assert len(hand) == 2, 'All predefined hands must contain exactly 2 cards'
+                # assert len(hand) == 2, 'All predefined hands must contain exactly 2 cards'
                 player.hand = hand
 
             self.dealer.hand = dealer_hand
@@ -205,9 +215,7 @@ class Blackjack:
         is_finished = True
         player = None
         while is_finished:
-            if self.player_turn == self.n_players:
-                self.player_turn = 0
-            player = self.players[self.player_turn]
+            player = self.players[self.player_turn % self.n_players]
             is_finished = player.is_finished
             self.player_turn += 1
 
@@ -247,7 +255,7 @@ class Blackjack:
     def hit(self) -> None:
         return self.__hit(self.next_player())
 
-    def __hit(self, player=None) -> None:
+    def __hit(self, player: Player = None) -> None:
         if player is None: return
         assert player.hand.value != 21, f'Cannot hit on {player.hand}!'
 
@@ -262,7 +270,7 @@ class Blackjack:
     def stand(self) -> None:
         return self.__stand(self.next_player())
 
-    def __stand(self, player) -> None:
+    def __stand(self, player: Player) -> None:
         player.log_decision(GameDecision.STAND)
         player.status = GameState.STAND
 
@@ -284,11 +292,13 @@ class Blackjack:
 
         self.player_turn += 1  # account for new hand
 
-    def play_random(self) -> None:
+    def play_random(self) -> str:
         player = self.next_player()
-        if player is None or player.status == GameState.STAND: return
+        if player is None or player.status == GameState.STAND: return ''
 
-        if player.hand.value == 21:
+        if player.hand.value <= 11:  # doesn't make sense not to HIT for these hands. Impossible to lose if you hit!
+            decision = GameDecision.HIT
+        elif player.hand.value == 21:
             decision = GameDecision.STAND
         elif player.hand.is_splittable:
             decision = choice([GameDecision.STAND, GameDecision.HIT, GameDecision.SPLIT])
@@ -302,6 +312,8 @@ class Blackjack:
         elif decision == GameDecision.SPLIT:
             self.__split(player)
 
+        return decision
+
     def play_full_random(self) -> None:
         while not self.is_finished:
             self.play_random()
@@ -314,21 +326,10 @@ class Blackjack:
     def n_players(self) -> int:
         return len(self.players)
 
-    def get_players_profit(self) -> defaultdict:
-        players = defaultdict(int)
-
-        for player in self.players:
-            if player.status == GameState.WON and player.is_blackjack:
-                player.payout = self.blackjack_payout
-            elif player.status == GameState.WON:
-                player.payout = 1
-            elif player.status == GameState.DRAW:
-                player.payout = 0
-            elif player.status == GameState.LOST:
-                player.payout = -1
-
-            players[player.name] += player.payout
-
+    def get_players_profit(self) -> dict:
+        # waaay too complicated!!
+        player_names = np.unique([player.name for player in self.players])
+        players = {pn: sum(p.profit for p in self.players if p.name == pn) for pn in player_names}
         return players
 
     def __repr__(self):
@@ -341,64 +342,78 @@ class Blackjack:
 
 
 def get_best_decision(x: dict):
-    hit_exp = x[GameDecision.HIT]['profit'] / x[GameDecision.HIT]['n_occ']# if x[GameDecision.HIT]['n_occ'] > 0 else -10e100
-    stand_exp = x[GameDecision.STAND]['profit'] / x[GameDecision.STAND]['n_occ']# if x[GameDecision.STAND]['n_occ'] > 0 else -10e100
+    hit_exp = x[GameDecision.HIT]['profit'] / x[GameDecision.HIT]['n_occ'] if x[GameDecision.HIT]['n_occ'] > 0 else -10e100
+    stand_exp = x[GameDecision.STAND]['profit'] / x[GameDecision.STAND]['n_occ'] if x[GameDecision.STAND]['n_occ'] > 0 else -10e100
     split_exp = x[GameDecision.SPLIT]['profit'] / x[GameDecision.SPLIT]['n_occ'] if x[GameDecision.SPLIT]['n_occ'] > 0 else -10e100
     return [GameDecision.HIT, GameDecision.STAND, GameDecision.SPLIT][np.argmax([hit_exp, stand_exp, split_exp])]
 
 
 def _get_basic_strategy():
+    # TODO: run simulations in parallel
     pass
 
 
-def get_basic_strategy(n_sims: int = 100_000):
+def get_basic_strategy(n_sims: int = 10_000):
+    splittable_hands = [f'{c},{c}' for c in 'A23456789T']
+    soft_hands = [f'A,{c}' for c in 'A23456789'[1:]]
+    hard_hands = list(np.arange(12, 21))  # hands < 12 should obviously HIT
 
-    player_starting_vals = list(np.arange(5, 22)) + [f'{c},{c}' for c in 'A23456789T'] + [f'A,{c}' for c in 'A23456789T'[1:]]
-    dealer_starting_vals = range(2, 12)
+    player_starting_vals = hard_hands + splittable_hands + soft_hands
+    dealer_starting_vals = list(np.arange(2, 12))
     def_val = {'profit': 0, 'n_occ': 0}
     outcomes = {
         k: {GameDecision.HIT: def_val.copy(), GameDecision.STAND: def_val.copy(), GameDecision.SPLIT: def_val.copy()}
-        for k in product(player_starting_vals, dealer_starting_vals)
+        for k in product(hard_hands, dealer_starting_vals)
+        # for k in product(player_starting_vals, dealer_starting_vals)
     }
 
-    for i in range(1, n_sims + 1):
-        if i % 5_000 == 0: print(f'{int(i/n_sims*100)}%')
-        game = Blackjack(n_players=10)
+    for player_val, dealer in outcomes:
+        print(player_val, dealer)
+        if isinstance(player_val, str):
+            player_hand = ''.join(player_val.split(','))
+        else:
+            c1 = player_val // 2 + 1
+            c2 = player_val // 2 - 1 if player_val % 2 == 0 else player_val // 2
+            if c1 == 10: c1 = 'T'
+            if c2 == 10: c2 = 'T'
+            player_hand = f'{c1}{c2}'
 
-        game.play_full_random()
+            if player_val == 20: player_hand = '884'
 
-        dealer_first_card = 11 if game.dealer.hand.cards[0] == 'A' else game.dealer.hand.cards[0].value
+        if dealer == 10:
+            dealer_hand = 'T'
+        elif dealer == 11:
+            dealer_hand = 'A'
+        else:
+            dealer_hand = str(dealer)
 
-        players_profit = game.get_players_profit()
-        for player in game.players:
-            hand, decision = player.decision_hist[-1]
-            split_hand = next((hand for hand, decision in player.decision_hist[::-1] if decision == GameDecision.SPLIT), None)
+        decisions = [GameDecision.HIT, GameDecision.STAND]
 
-            if split_hand is not None:
-                c = 'T' if split_hand.cards[0].rank in 'JQK' else split_hand.cards[0]
-                key_split = '{card},{card}'.format(card=c)
-                decision_split = GameDecision.SPLIT
+        # if len(set(player_hand)) == 1: decisions.append(GameDecision.SPLIT)
+        for decision in decisions:
+            for _ in range(n_sims):
+                game = Blackjack(player_hands=[Hand(player_hand)], dealer_hand=Hand(dealer_hand))
+                if decision == GameDecision.HIT: game.hit()
+                if decision == GameDecision.STAND: game.stand()
+                # if decision == GameDecision.SPLIT: game.split()
 
-                outcomes[(key_split, dealer_first_card)][decision_split]['profit'] += players_profit[player.name]
-                outcomes[(key_split, dealer_first_card)][decision_split]['n_occ'] += 1
+                if not game.is_finished and game.players[0].status != GameState.STAND:
+                    game.stand()
 
-            if hand.is_splittable:
-                c1 = 'T' if hand.cards[0].rank in 'JQK' else hand.cards[0]
-                c2 = 'T' if hand.cards[1].rank in 'JQK' else hand.cards[1]
-                key = f'{c1},{c2}'
-            elif hand.is_soft_value:
-                c = hand.cards[0] if hand.cards[1].rank == 'A' else hand.cards[1]
-                c = 'T' if c.rank in 'JQK' else c
-                key = f'A,{c}'
-            else:
-                key = hand.value
+                if not game.is_finished:
+                    game.hit_dealer()
 
-            outcomes[(key, dealer_first_card)][decision]['profit'] += players_profit[player.name]
-            outcomes[(key, dealer_first_card)][decision]['n_occ'] += 1
+                players_profit = game.get_players_profit()
+
+                player = game.players[0]
+
+                outcomes[(player_val, dealer)][decision]['profit'] += players_profit[player.name]
+                outcomes[(player_val, dealer)][decision]['n_occ'] += 1
 
     pprint(outcomes)
 
     best_play = {hands: get_best_decision(outcomes[hands]) for hands, _ in outcomes.items()}
+
     df_best_play = pd.DataFrame(best_play.values(), index=list(best_play.keys()))
     df_best_play.index = pd.MultiIndex.from_tuples(df_best_play.index, names=['player', 'dealer'])
     df_best_play = df_best_play.unstack().replace({1: GameDecision.HIT, 0: GameDecision.STAND})
@@ -421,7 +436,6 @@ def simulate_hand(players: List[Hand], dealer: Hand, n_sims=10_000):
         status = game.players[0].status
         print(game)
         print(decision, status)
-
         res[decision] += game.get_players_profit()[0]
 
     print('Expected Profit')
@@ -430,6 +444,6 @@ def simulate_hand(players: List[Hand], dealer: Hand, n_sims=10_000):
 
 
 if __name__ == '__main__':
-    simulate_hand([Hand('23')], Hand('2'))
-    df = get_basic_strategy(n_sims=10_000)
+    # simulate_hand([Hand('23')], Hand('2'))
+    df = get_basic_strategy(n_sims=1_000)
     pprint(df)
